ddb44160aafa701408f1bf0fb0851824add2484b
Rohan1885633 <141541061+Rohan1885633@users.noreply.github.com>
2024-03-20 15:22:47 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,17 +20,17 @@ class script(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📢 Channel :</b> <a href='https://t.me/The_Movies_Officially</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/ROHANKUMAR188'>RJ SUPPORT</a>
 
-<b>♻️ Bot Made By :</b> @Madflix_Bots"""
+<b>♻️ Bot Made By :</b>@ROHANKUMAR188"""
 
     DONATE_TXT = """
 <b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>madflixofficial@axl</code> 
+<b>🛍 UPI ID:</b> <code>6371434453@fam</code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
